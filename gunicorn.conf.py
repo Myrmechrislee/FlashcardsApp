341d6a2e0676a0a835a1e921745a3cb5394b6cdd
Christophe Lee <christophelee@christophes-mbp.lan>
2025-02-25 14:46:22 +1100

    Remove HTTPS for caddy later

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,12 +3,10 @@ import multiprocessing
 
 max_requests = 1000
 max_requests_jitter = 50
-certfile = '/etc/letsencrypt/live/flash-cards.clintonalee.com/fullchain.pem'
-keyfile = '/etc/letsencrypt/live/flash-cards.clintonalee.com/privkey.pem'
 
 log_file = "-"
 
-bind = "0.0.0.0:443"
+bind = "0.0.0.0:8080"
 
 workers = (multiprocessing.cpu_count() * 2) + 1
 threads = workers
